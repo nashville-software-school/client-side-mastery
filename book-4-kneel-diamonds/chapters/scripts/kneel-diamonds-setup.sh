ba909671c5d6eef74729239bfda1b9a3e1e2336b
Steve Brownlee <chortlehoort@gmail.com>
2021-06-07 16:11:03 -0500

    Included imports added by Joe to make initial error match video

--- book-4-kneel-diamonds/chapters/scripts/kneel-diamonds-setup.sh
+++ book-4-kneel-diamonds/chapters/scripts/kneel-diamonds-setup.sh
@@ -126,6 +126,8 @@ export const getMetals = () => {
 
 echo '
 import { DiamondSizes } from "./DiamondSizes.js"
+import { JewelryStyles } from "./JewelryStyles.js"
+import { Orders } from "./Orders.js"
 
 document.addEventListener(
     "click",
