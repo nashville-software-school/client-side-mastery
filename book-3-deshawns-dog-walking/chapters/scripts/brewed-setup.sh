2b8de5507e3597296bfb2b2cfd55cc2948ac10d2
Steve Brownlee <chortlehoort@gmail.com>
2021-07-22 12:06:17 -0500

    Added primary keys for orders in BA

--- book-3-deshawns-dog-walking/chapters/scripts/brewed-setup.sh
+++ book-3-deshawns-dog-walking/chapters/scripts/brewed-setup.sh
@@ -182,39 +182,39 @@ const database = {
         id: 10, name: "Cappucino", price: 12.49
     }],
     orders: [{
-        productId: 10, employeeId: 5, timestamp: 1613538111396
+        id: 1, productId: 10, employeeId: 5, timestamp: 1613538111396
     }, {
-        productId: 5, employeeId: 1, timestamp: 1613038102396
+        id: 2, productId: 5, employeeId: 1, timestamp: 1613038102396
     }, {
-        productId: 2, employeeId: 7, timestamp: 1612837112396
+        id: 3, productId: 2, employeeId: 7, timestamp: 1612837112396
     }, {
-        productId: 8, employeeId: 5, timestamp: 1612836112396
+        id: 4, productId: 8, employeeId: 5, timestamp: 1612836112396
     }, {
-        productId: 9, employeeId: 7, timestamp: 1612735112396
+        id: 5, productId: 9, employeeId: 7, timestamp: 1612735112396
     }, {
-        productId: 4, employeeId: 1, timestamp: 1614528112396
+        id: 6, productId: 4, employeeId: 1, timestamp: 1614528112396
     }, {
-        productId: 5, employeeId: 8, timestamp: 1614598112396
+        id: 7, productId: 5, employeeId: 8, timestamp: 1614598112396
     }, {
-        productId: 2, employeeId: 9, timestamp: 1614630112396
+        id: 8, productId: 2, employeeId: 9, timestamp: 1614630112396
     }, {
-        productId: 3, employeeId: 2, timestamp: 1612538112396
+        id: 9, productId: 3, employeeId: 2, timestamp: 1612538112396
     }, {
-        productId: 9, employeeId: 6, timestamp: 1612438112396
+        id: 10, productId: 9, employeeId: 6, timestamp: 1612438112396
     }, {
-        productId: 4, employeeId: 1, timestamp: 1612338112396
+        id: 11, productId: 4, employeeId: 1, timestamp: 1612338112396
     }, {
-        productId: 6, employeeId: 11, timestamp: 1612238112396
+        id: 12, productId: 6, employeeId: 11, timestamp: 1612238112396
     }, {
-        productId: 10, employeeId: 8, timestamp: 1630538112396
+        id: 13, productId: 10, employeeId: 8, timestamp: 1630538112396
     }, {
-        productId: 9, employeeId: 9, timestamp: 1612738112396
+        id: 14, productId: 9, employeeId: 9, timestamp: 1612738112396
     }, {
-        productId: 3, employeeId: 3, timestamp: 1612938112396
+        id: 15, productId: 3, employeeId: 3, timestamp: 1612938112396
     }, {
-        productId: 4, employeeId: 7, timestamp: 1612638112396
+        id: 16, productId: 4, employeeId: 7, timestamp: 1612638112396
     }, {
-        productId: 4, employeeId: 10, timestamp: 1612638112396
+        id: 17, productId: 4, employeeId: 10, timestamp: 1612638112396
     }]
 }
 
