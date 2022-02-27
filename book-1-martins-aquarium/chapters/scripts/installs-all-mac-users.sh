f83a42e621f9174177837d9da89513ee3059e83f
Steve <chortlehoort@gmail.com>
2022-02-26 22:03:40 -0600

    Added Node install and zsh check

--- book-1-martins-aquarium/chapters/scripts/installs-all-mac-users.sh
+++ book-1-martins-aquarium/chapters/scripts/installs-all-mac-users.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# set -u
 
 clear
 echo -e "\n   ************************************************"
@@ -14,46 +13,71 @@ read -p "Enter email address you used for Github: " emailAddress
 
 
 # Set up workspace directory
-echo -e "\n\n Creating some directories that you will need..."
+echo -e "\n\nCreating some directories that you will need..."
 mkdir -p $HOME/workspace
 mkdir -p $HOME/.ssh
 mkdir -p $HOME/.config
 mkdir -p $HOME/.npm-packages
 
 # Create SSH key
-echo -e "\n\n Generating an SSH key so you can backup your code to Github..."
+echo -e "\n\nGenerating an SSH key so you can backup your code to Github..."
 ssh-keygen -t rsa -f ~/.ssh/id_nss -N "" -b 4096 -C $emailAddress
 eval `ssh-agent`
 ssh-add ~/.ssh/id_nss
 
 # Install required package from Brew
-echo -e "\n\n Installing Visual Studio Code..."
+echo -e "\n\nInstalling Visual Studio Code..."
 brew install -q --cask visual-studio-code
 
-echo -e "\n\n Installing git and terminal customization tools..."
+echo -e "\n\nInstalling git and terminal customization tools..."
 brew install -q git tig zsh zsh-completions
+
+# Check if zsh is default shell. Switch if not.
+current_shell=$(echo $SHELL)
+if [ $current_shell == "/bin/bash" ];
+then
+    echo "\n\n\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "Change Needed: "
+    echo "Switching from bash to zsh as default shell for terminal."
+    echo "This might require you to enter your computer password."
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+    chsh -s /bin/zsh
+else
+    echo "Already using zsh as default shell"
+fi
 
 # User settings for git
 git config --global user.name $studentName
 git config --global user.email $emailAddress
 
 # Install ohmyzsh
-echo -e "\n\n Installing more terminal customization tools..."
+echo -e "\n\nInstalling more terminal customization tools..."
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Show hidden file by default in Finder
-echo -e "\n\n Configuring the Finder application to show hidden files..."
+echo -e "\n\nConfiguring the Finder application to show hidden files..."
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
 
+# Install Node
+echo -e "\n\n\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "Installation Needed: Node.js"
+echo "This installation might require your computer password."
+echo -e "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+
+NODE_VERSION=$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')
+curl "https://nodejs.org/dist/latest/node-$NODE_VERSION.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
+
 # Install global dependencies
-echo -e "\n\n Installing a web server and a simple API server..."
+echo -e "\n\nInstalling a web server and a simple API server..."
 npm config set prefix $HOME/.npm-packages
 echo 'export PATH="$PATH:$HOME/.npm-packages/bin"' >> ~/.zshrc
-echo -e "\n\n Reloading your newly configured terminal..."
+echo -e "\n\nReloading your newly configured terminal..."
 source ~/.zshrc &>zsh-reload.log
-npm i -g serve json-server serve
+npm i -g serve json-server
+
 
 # Get latest Xcode
-echo -e "\n\n Making sure you have the latest Xcode compiler..."
+echo -e "\n\nMaking sure you have the latest Xcode compiler..."
 xcode-select --install
