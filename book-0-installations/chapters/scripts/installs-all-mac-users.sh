04ccd72a9129a7849ff3db9de79f81dc894e5f4b
Steve <chortlehoort@gmail.com>
2023-03-13 11:44:02 -0500

    Install oh-my-zsh last

--- book-0-installations/chapters/scripts/installs-all-mac-users.sh
+++ book-0-installations/chapters/scripts/installs-all-mac-users.sh
@@ -17,53 +17,89 @@ read -p "> " githubUsername
 echo -e "\nEnter personal access token for Github:"
 read -s -p "> " githubPassword
 
-
 # Set up workspace directory
-echo -e "\n\nCreating some directories that you will need..."
-mkdir -p $HOME/workspace
-mkdir -p $HOME/.ssh
-mkdir -p $HOME/.config
-mkdir -p $HOME/.npm-packages
+FOLDER=$HOME/workspace
+if [ ! -d "$FOLDER" ]; then
+  echo -e "\n\nCreating some directories that you will need..."
+  mkdir -p $HOME/workspace
+  mkdir -p $HOME/.ssh
+  mkdir -p $HOME/.config
+  mkdir -p $HOME/.npm-packages
+else
+  echo "Skipping directory creation"
+fi
 
-# Create SSH key
-echo -e "\n\nGenerating an SSH key so you can backup your code to Github..."
-echo "yes" | ssh-keygen -t rsa -f ~/.ssh/id_nss -N "" -b 4096 -C $emailAddress
-eval `ssh-agent`
-ssh-add ~/.ssh/id_nss
-echo -e "Host *\n\tAddKeysToAgent yes\n\tIdentityFile ~/.ssh/id_nss" >> ~/.ssh/config
 
-# Add SSH key to Github account
-echo -e "\n\nAdding your SSH key to your Github account..."
-PUBLIC_KEY=$(cat $HOME/.ssh/id_nss.pub)
-curl \
-  -X POST \
-  -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.github.v3+json" \
-  -u "$githubUsername:$githubPassword" \
-  https://api.github.com/user/keys \
-  -d "{\"key\":\"$PUBLIC_KEY\",\"title\":\"NSS Automated Key\"}"
+# Mac Specific installs
+if ! command -v brew &> /dev/null; then
+  echo -e "\n\n\n\n Installing Homebrew..."
 
-# Install Homebrew
-echo -e "\n\n\n\n"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@                                                            @@"
-echo "@@   Installation Needed: Homebrew                            @@"
-echo "@@   This installation will require your computer password.   @@"
-echo "@@                                                            @@"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-export PATH="/usr/local/bin:$PATH"
-echo 'export PATH="/usr/local/bin:$PATH"' >> $HOME/.profile
-
-# Install required package from Brew
-echo -e "\n\nInstalling Visual Studio Code..."
-brew install -q --cask visual-studio-code
+  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$USER/.zprofile
+  eval $(/opt/homebrew/bin/brew shellenv)
+fi
 
 echo -e "\n\nInstalling git and terminal customization tools..."
 brew install -q git tig zsh zsh-completions
 
-# Check if zsh is default shell. Switch if not.
+echo -e "\n\nConfiguring the Finder application to show hidden files..."
+defaults write com.apple.finder AppleShowAllFiles YES
+killall Finder
+# End Mac Specific
+
+# Set up git and github - Needs to be below the brew install
+PUBLIC_KEY=$HOME/.ssh/id_nss.pub
+if [ ! -f "$PUBLIC_KEY" ]; then
+  echo -e "\n\nGenerating an SSH key so you can backup your code to Github..."
+  echo "yes" | ssh-keygen -t rsa -f ~/.ssh/id_nss -N "" -b 4096 -C $emailAddress
+  eval `ssh-agent`
+  ssh-add ~/.ssh/id_nss
+  echo -e "Host *\n\tAddKeysToAgent yes\n\tIdentityFile ~/.ssh/id_nss" >> ~/.ssh/config
+fi
+
+echo -e "\n\nAdding your SSH key to your Github account..."
+PUBLIC_KEY_CONTENT=$(cat $HOME/.ssh/id_nss.pub)
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $githubPassword"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/user/keys \
+  -d "{\"key\":\"$PUBLIC_KEY_CONTENT\",\"title\":\"NSS Automated Key\"}"
+
+git config --global user.name "$studentName"
+git config --global user.email $emailAddress
+# End Git set up
+
+# Install Node - Needs to be below zsh set up because of the shell environment
+if ! command -v nvm &> /dev/null; then
+  echo -e "Installing Node Version Manager..."
+
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  source ~/.zshrc &>zsh-reload.log
+fi
+
+nvm install --lts
+nvm use --lts
+
+echo -e "\n\nInstalling a web server and a simple API server..."
+npm config set prefix $HOME/.npm-packages
+echo 'export PATH="$PATH:$HOME/.npm-packages/bin"' >> ~/.zshrc
+source ~/.zshrc &>zsh-reload.log
+npm i -g serve json-server cypress
+# End Node Set up
+
+# Install XCode Command line tools - May take awhile so run this one last
+echo -e "\n\nMaking sure you have Command line tools installed"
+xcode-select --install
+
+# Set up Zsh
+ZSH_FOLDER=$HOME/.oh-my-zsh
+if [ ! -d "$FOLDER" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 current_shell=$(echo $SHELL)
 if [ $current_shell == "/bin/bash" ];
 then
@@ -74,45 +110,19 @@ then
     echo "@@   This change might require your computer password.    @@"
     echo "@@                                                        @@"
     echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-    chsh -s /bin/zsh
+    ZSH_PATH=$(which zsh)
+    chsh -s $ZSH_PATH
+
+    new_shell=$(echo $SHELL)
+    if [ $new_shell != "$ZSH_PATH" ]; then
+      # The rest of the installs will not work if zsh is not the default shell
+      echo "Shell did not change to zsh. Reach out to an instructor before continuing"
+      exit
+    fi
 else
     echo "Already using zsh as default shell"
 fi
-
-# User settings for git
-git config --global user.name "$studentName"
-git config --global user.email $emailAddress
-
-# Install ohmyzsh
-echo -e "\n\nInstalling more terminal customization tools..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Show hidden file by default in Finder
-echo -e "\n\nConfiguring the Finder application to show hidden files..."
-defaults write com.apple.finder AppleShowAllFiles YES
-killall Finder
-
-# Install Node
-echo -e "\n\n\n\n"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@                                                             @@"
-echo "@@   Installation Needed: Node.js                              @@"
-echo "@@   This installation might require your computer password.   @@"
-echo "@@                                                             @@"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-brew install node
-
-# Install global dependencies
-echo -e "\n\nInstalling a web server and a simple API server..."
-npm config set prefix $HOME/.npm-packages
-echo 'export PATH="$PATH:$HOME/.npm-packages/bin"' >> ~/.zshrc
-source ~/.zshrc &>zsh-reload.log
-npm i -g serve json-server
-
-# Get latest Xcode
-echo -e "\n\nMaking sure you have the latest Xcode compiler..."
-xcode-select --install
+# End zsh set up
 
 echo -e "\n\n\n\n"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
