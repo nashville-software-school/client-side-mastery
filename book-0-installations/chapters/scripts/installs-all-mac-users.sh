04029c07f5a76c593cc85d8a85f85f236995cdc6
Steve Brownlee <chortlehoort@gmail.com>
2023-05-07 00:22:40 -0500

    Updating install script to use functions, with long running ones pushed to background parallael processes

--- book-0-installations/chapters/scripts/installs-all-mac-users.sh
+++ book-0-installations/chapters/scripts/installs-all-mac-users.sh
@@ -17,6 +17,7 @@ read -p "> " GH_USERNAME
 echo -e "\nEnter personal access token for Github (it will be invisible because security):"
 read -s -p "> " GH_PWD
 
+
 # Set up standard directories
 echo -e "\n\nCreating some directories that you will need..."
 DIRS=("$HOME/workspace" "$HOME/.ssh" "$HOME/.config" "$HOME/.npm-packages")
@@ -28,114 +29,158 @@ for DIR in "${DIRS[@]}"; do
   fi
 done
 
-# Mac Specific installs
-if ! type brew &>/dev/null; then
-  echo -e "\n\n\n\n Installing Homebrew..."
+function installxcode() {
+  # Install XCode Command line tools - May take awhile so run this one last
+  echo -e "\n\nMaking sure you have Command line tools installed"
+  xcode-select --install >>/dev/null
+}
 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+function installbrew() {
+  if ! type brew &>/dev/null; then
+    echo -e "\n\n\n\n Installing Homebrew..."
 
-  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >>/Users/$USER/.zprofile
-  eval $(/opt/homebrew/bin/brew shellenv)
-fi
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo -e "\n\nInstalling git and terminal customization tools..."
-brew install -q git tig zsh zsh-completions >>/dev/null 2>>error.log
-
-echo -e "\n\nConfiguring the Finder application to show hidden files..."
-defaults write com.apple.finder AppleShowAllFiles YES
-killall Finder >>/dev/null 2>>error.log
-# End Mac Specific
-
-# Set up git and github - Needs to be below the brew install
-PUBLIC_KEY=$HOME/.ssh/id_nss.pub
-if [ ! -f "$PUBLIC_KEY" ]; then
-  echo -e "\n\nGenerating an SSH key so you can backup your code to Github..."
-  echo "yes" | ssh-keygen -t ed25519 -f ~/.ssh/id_nss -N "" -b 4096 -C $EMAIL_ADDRESS
-  eval $(ssh-agent)
-  ssh-add ~/.ssh/id_nss
-  echo -e "Host *\n\tAddKeysToAgent yes\n\tIdentityFile ~/.ssh/id_nss" >>~/.ssh/config
-fi
-
-echo -e "\n\nAdding your SSH key to your Github account..."
-PUBLIC_KEY_CONTENT=$(cat $HOME/.ssh/id_nss.pub)
-
-STATUS_CODE=$(curl \
-  -s \
-  --write-out %{http_code} \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GH_PWD" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/user/keys \
-  -d "{\"key\":\"$PUBLIC_KEY_CONTENT\",\"title\":\"NSS Automated Key\"}" >>progress.log 2>>error.log)
-
-if [ ! $STATUS_CODE == 200 ]; then
-  echo -e "POST for SSH key returned status code $STATUS_CODE" >>error.log
-fi
-
-git config --global user.name "$STUDENT_NAME" >>/dev/null 2>>error.log
-git config --global user.email $EMAIL_ADDRESS >>/dev/null 2>>error.log
-# End Git set up
-
-# Install XCode Command line tools - May take awhile so run this one last
-echo -e "\n\nMaking sure you have Command line tools installed"
-xcode-select --install >>/dev/null 2>>error.log
-
-# Set up Zsh
-
-current_shell=$(echo $SHELL)
-if [ $current_shell == "/bin/bash" ]; then
-  echo -e "\n\n\n"
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  echo "@@                                                        @@"
-  echo "@@   Change Needed: Switch to zsh                         @@"
-  echo "@@   This change might require your computer password.    @@"
-  echo "@@                                                        @@"
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  ZSH_PATH=$(which zsh >>progress.log 2>>error.log)
-  if [ $? != 0 ]; then
-    echo "FAILED: zsh not found. Skipping chsh" >>error.log
-  else
-    SWITCHED=$(chsh -s $ZSH_PATH >>progress.log 2>>error.log)
-    if [ $? != 0 ]; then
-      echo "FAILED: Could not chsh to zsh" >>error.log
-    else
-      new_shell=$(echo $SHELL)
-      if [ $new_shell != "$ZSH_PATH" ]; then
-        # The rest of the installs will not work if zsh is not the default shell
-        echo "Shell did not change to zsh. Reach out to an instructor before continuing"
-        exit
-      fi
-    fi
+    echo 'eval $(/opt/homebrew/bin/brew shellenv)' >>/Users/$USER/.zprofile
+    eval $(/opt/homebrew/bin/brew shellenv)
   fi
 
-else
-  echo "Already using zsh as default shell"
-fi
+  echo -e "\n\nInstalling git and terminal customization tools..."
+  brew install -q git tig zsh zsh-completions >>/dev/null
+}
 
-ZSH_FOLDER=$HOME/.oh-my-zsh
-if [ ! -d "$FOLDER" ]; then
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh )" >>/dev/null 2>>error.log
-fi
-# End zsh set up
+function setupgit() {
+  # Set up git and github - Needs to be below the brew install
+  PUBLIC_KEY=$HOME/.ssh/id_nss.pub
+  if [ ! -f "$PUBLIC_KEY" ]; then
+    echo -e "\n\nGenerating an SSH key so you can backup your code to Github..."
+    echo "yes" | ssh-keygen -t ed25519 -f ~/.ssh/id_nss -N "" -b 4096 -C $EMAIL_ADDRESS
+    eval $(ssh-agent)
+    ssh-add ~/.ssh/id_nss
+    echo -e "Host *\n\tAddKeysToAgent yes\n\tIdentityFile ~/.ssh/id_nss" >>~/.ssh/config
+  fi
 
-# Install Node - Needs to be below zsh set up because of the shell environment
-if ! type nvm &>/dev/null; then
-  echo -e "Installing Node Version Manager..."
+  echo -e "\n\nAdding your SSH key to your Github account..."
+  PUBLIC_KEY_CONTENT=$(cat $HOME/.ssh/id_nss.pub)
 
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash >>/dev/null 2>>error.log
+  STATUS_CODE=$(curl \
+    -s \
+    --write-out %{http_code} \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $GH_PWD" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/user/keys \
+    -d "{\"key\":\"$PUBLIC_KEY_CONTENT\",\"title\":\"NSS Automated Key\"}" >>/dev/null)
+
+  if [ ! $STATUS_CODE == 200 ]; then
+    echo -e "POST for SSH key returned status code $STATUS_CODE" >>progress.log
+  fi
+
+  git config --global user.name "$STUDENT_NAME" >>/dev/null
+  git config --global user.email "$EMAIL_ADDRESS" >>/dev/null
+  # End Git set up
+
+}
+
+function showhidden() {
+  echo -e "\n\nConfiguring the Finder application to show hidden files..."
+  defaults write com.apple.finder AppleShowAllFiles YES
+  killall Finder >>/dev/null
+}
+
+function setupzsh() {
+  # Set up Zsh
+  current_shell=$(echo $SHELL)
+  if [ $current_shell == "/bin/bash" ]; then
+    echo -e "\n\n\n"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    echo "@@                                                        @@"
+    echo "@@   Change Needed: Switch to zsh                         @@"
+    echo "@@   This change might require your computer password.    @@"
+    echo "@@                                                        @@"
+    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+    ZSH_PATH=$(which zsh >>/dev/null)
+    if [ $? != 0 ]; then
+      echo "FAILED: zsh not found. Skipping chsh" >>error.log
+    else
+      SWITCHED=$(chsh -s $ZSH_PATH >>/dev/null)
+      if [ $? != 0 ]; then
+        echo "FAILED: Could not chsh to zsh" >>error.log
+      else
+        new_shell=$(echo $SHELL)
+        if [ $new_shell != "$ZSH_PATH" ]; then
+          # The rest of the installs will not work if zsh is not the default shell
+          echo "FAILED: Shell did not change to zsh." >>error.log
+          exit 1
+        fi
+      fi
+    fi
+
+  else
+    echo "Already using zsh as default shell"
+  fi
+
+  ZSH_FOLDER=$HOME/.oh-my-zsh
+  if [ ! -d "$FOLDER" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >>/dev/null
+  fi
+
+  if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  fi
+  if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  fi
+
+  if grep -q "(zsh-autosuggestions zsh-syntax-highlighting" ~/.zshrc; then
+    echo "Text already exists"
+  else
+    sed -i'.original' 's#^plugins=(#plugins=(zsh-autosuggestions zsh-syntax-highlighting #' $HOME/.zshrc
+  fi
+
+}
+
+function installnode() {
+  # Install Node - Needs to be below zsh set up because of the shell environment
+  if ! type nvm &>/dev/null; then
+    echo -e "Installing Node Version Manager..."
+
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash >>/dev/null
+    source ~/.zshrc &>zsh-reload.log
+  fi
+
+  nvm install --lts >>/dev/null
+  nvm use --lts >>/dev/null
+
+  echo -e "\n\nInstalling a web server and a simple API server..."
+  npm config set prefix $HOME/.npm-packages
+  echo 'export PATH="$PATH:$HOME/.npm-packages/bin"' >>~/.zshrc
   source ~/.zshrc &>zsh-reload.log
-fi
+  npm i -g serve json-server json >>/dev/null
+}
 
-nvm install --lts >>/dev/null 2>>error.log
-nvm use --lts >>/dev/null 2>>error.log
 
-echo -e "\n\nInstalling a web server and a simple API server..."
-npm config set prefix $HOME/.npm-packages
-echo 'export PATH="$PATH:$HOME/.npm-packages/bin"' >>~/.zshrc
-source ~/.zshrc &>zsh-reload.log
-npm i -g serve json-server cypress >>/dev/null 2>>error.log
-# End Node Set up
+spinner="/-\|"
+
+installbrew
+installnode
+
+showhidden &
+setupgit &
+setupzsh &
+
+installxcode &
+while ps -p $! > /dev/null; do
+    # Print the next character in the spinner sequence
+    printf "\b${spinner:0:1} Installing Xcode..."
+    # Rotate the spinner sequence
+    spinner="${spinner:1}${spinner:0:1}"
+    # Wait for a short time
+    sleep 0.5
+done
+
+wait
 
 # BEGIN verification
 echo -e "\n\n\n\n"
