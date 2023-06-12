d7cbb6679adfb15cd4284fae3b8966e22dd5d9b2
Valerie Freeman <valerah7@gmail.com>
2023-06-12 10:14:54 -0500

    fixed bash scripts for surf

--- book-1-queen-bee/chapters/scripts/averagePrice.sh
+++ book-1-queen-bee/chapters/scripts/averagePrice.sh
@@ -5,16 +5,16 @@ mkdir -p "$HOME/workspace/surf-shop"
 cd "$HOME/workspace/surf-shop"
 
 echo 'const inventory = [
-    { "id": 1, "description": "9 ft longboard surfboard", "price": 499.99, type: "surfboard" },
-    { "id": 2, "description": "5 ft shortboard surfboard", "price": 399.99, type: "surfboard" },
-    { "id": 3, "description": "Full wetsuit with hood and boots", "price": 199.99, type: "clothing" },
-    { "id": 4, "description": "Surfboard leash", "price": 29.99, type: "gear" },
-    { "id": 5, "description": "Surf wax", "price": 9.99, type: "gear" },
-    { "id": 6, "description": "Surfboard fins", "price": 39.99, type: "gear" },
-    { "id": 7, "description": "Rashguard shirt", "price": 49.99, type: "clothing" },
-    { "id": 8, "description": "Surfboard rack for car", "price": 99.99, type: "gear" },
-    { "id": 9, "description": "Beach towel", "price": 24.99, type: "clothing" },
-    { "id": 10, "description": "Surfboard repair kit", "price": 19.99, type: "gear" }
+    { id: 1, description: "9 ft longboard surfboard", price: 499.99, type: "surfboard" },
+    { id: 2, description: "5 ft shortboard surfboard", price: 399.99, type: "surfboard" },
+    { id: 3, description: "Full wetsuit with hood and boots", price: 199.99, type: "clothing" },
+    { id: 4, description: "Surfboard leash", price: 29.99, type: "gear" },
+    { id: 5, description: "Surf wax", price: 9.99, type: "gear" },
+    { id: 6, description: "Surfboard fins", price: 39.99, type: "gear" },
+    { id: 7, description: "Rashguard shirt", price: 49.99, type: "clothing" },
+    { id: 8, description: "Surfboard rack for car", price: 99.99, type: "gear" },
+    { id: 9, description: "Beach towel", price: 24.99, type: "clothing" },
+    { id: 10, description: "Surfboard repair kit", price: 19.99, type: "gear" }
 ]
 
 const isBargain = (inventoryItem) => {
